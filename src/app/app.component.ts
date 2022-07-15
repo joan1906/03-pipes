49fb2d1883dea97717e93cd4df8f8eb7456f70ff
Joan Blanco <joanblanco1906@gmail.com>
2022-07-15 12:02:36 -0400

    pipe decimal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nombre: string = 'Capitán América';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
 }
