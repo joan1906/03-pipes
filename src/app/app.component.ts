b7db4278378dc4df4eb193743d6158379b7d4184
Joan Blanco <joanblanco1906@gmail.com>
2022-07-15 14:09:19 -0400

    pipe percent and currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ export class AppComponent {
   nombre: string = 'Capitán América';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
 }
