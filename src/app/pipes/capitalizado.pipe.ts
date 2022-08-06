efb96f7375e8884f5777a932b6d44a4337222dce
Joan Blanco <joanblanco1906@gmail.com>
2022-08-06 00:33:05 -0400

    pipe para capitalizar nombre

--- src/app/pipes/capitalizado.pipe.ts
+++ src/app/pipes/capitalizado.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizadoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): string {
-    return 'Hola Mundo';
+  transform(value: string, todas: boolean = true): string {
+    value = value.toLowerCase();
+    let nombres = value.split(' ');
+
+    if (todas) {
+      nombres = nombres.map(nombre => {return nombre[0].toUpperCase() + nombre.substr(1)});
+    } else {
+      nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substr(1); 
+    }
+    return nombres.join(' ');
   }
 
 }
